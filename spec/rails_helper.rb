13b944611625ab2fb8c248bd8943a48b5ea05a17
Muhammad Ardhi <xardhix@gmail.com>
2020-10-27 21:06:16 +0700

    fix spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,34 +3,12 @@ require 'database_cleaner/active_record'
 DatabaseCleaner.strategy = :truncation
 
 # [...]
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
-Shoulda::Matchers.configure do |config| 
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
 
 # [...]
-RSpec.configure do |config|
+# RSpec.configure do |config|
   # [...]
-  # add `FactoryBot` methods
-  config.include FactoryBot::Syntax::Methods
-
-  # start by truncating all the tables but then use the faster transaction strategy the rest of the time
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-    DatabaseCleaner.cleaner = :truncation
-  end
-
-  # start the transaction strategy as examples are run
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
-  end
   # [...]
-end
+# end
 
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
@@ -41,6 +19,14 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config| 
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -96,4 +82,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
